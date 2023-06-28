534657f1aab80866209161a03ea8e2aa40feb84d
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-06-28 02:35:47 +0000

    simplified but not working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import { useState } from "react";
-import ListOfTasks from "./listOfTasks";
-
-
 
 //create your first component
 const Home = () => {
 
-	const [tasks, setTask] = useState(["a","b","c"]);
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState("");
+	const [id, setId] = useState(0);
 
-	function eraseTask(taskDescription){
-		let new_tasks = [];
-		for (let index in tasks){
-			if (tasks[index] != taskDescription) new_tasks.push(tasks[index]);
-		}
-		setTask(new_tasks);
-	}
-
-	window.addEventListener("click", (e) => {
-		if (e.target.type == "button"){
-			let taskDescription = e.target.parentElement.parentElement.id;
-			eraseTask(taskDescription);
-			console.log(taskDescription);
-		}
-		
+	window.addEventListener("keypress", (e) => {
+		if (e.key == "Enter")
+			console.log(e.key);
 	});
+
+	function form(){
+		return (
+			<input 
+				type="text"
+				className="form-control"
+				placeholder="Enter New Task"
+				value={newTask}
+				onChange={e => setNewTask(e.target.value)}
+			/>
+		);
+	}
 
 	return (
 		<div className="container">
@@ -32,7 +31,14 @@ const Home = () => {
 				<h1 className="display-2 fst-italic text-center mt-5">todos</h1>
 			</div>
 			<div className="container-fluid">
-				<ListOfTasks myTasks = {tasks}/>
+				{form()}
+			</div>
+			<div className="container-fluid">
+				<ul className="list-group">
+					{tasks.map(task => {
+						return <li className="list-group-item" key={task[1]}> <ListElement taskDescription = {task[0]}/> </li>
+					})}
+				</ul>
 			</div>
 		</div>
 	);
