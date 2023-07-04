862a185dccb8eeed6f47e7a0887697f4a2d1f771
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-07-04 03:38:40 +0000

    delete is no happening

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ListElement from "./listElement";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
@@ -8,9 +8,69 @@ const Home = () => {
 	const [tasks, setTasks] = useState([]);
 	const [newTask, setNewTask] = useState("");
 
+	useEffect(() =>{
+
+		let getList = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			PARAMS: "NONE"
+		};
+
+		let createList = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify([])
+		};
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/roalfonsin", getList)
+		.then( r => {
+			return r.json();
+		})
+		.then(response => {
+			if (response.msg == "This use does not exists, first call the POST method first to create the list for this username"){
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/roalfonsin", createList)
+			}
+			else{
+				console.log(response);
+				let myTasks = [];
+				for (let index in response){
+					myTasks.push(response[index].label);
+				}
+				setTasks(myTasks);
+			}
+		});
+
+	},[])
+
 	function addTask(e){
+
 		if (e.key == "Enter")
 			if (newTask != ""){
+				let formatTasksForServer = [];
+				for (let aTask in tasks){
+					formatTasksForServer.push({label: tasks[aTask], done: false});
+				}
+				formatTasksForServer.push({label: newTask, done: false});
+				let updateList = {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(formatTasksForServer)
+				};
+				console.log(updateList.body);
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/roalfonsin", updateList)
+				.then (r => {
+					return r.json();
+				})
+				.then (response => {
+					console.log(response);
+				});
+				
 				setTasks([...tasks, newTask]);
 				setNewTask("");
 			}
@@ -23,6 +83,28 @@ const Home = () => {
 			for (let index in tasks)
 				if (indexTask != index)
 					newTasks.push(tasks[index]);
+			
+			let formatTasksForServer = [];
+			for (let aTask in newTasks){
+				formatTasksForServer.push({label: tasks[aTask], done: false});
+			}
+			let updateList = {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formatTasksForServer)
+			};
+			console.log(updateList.body);
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/roalfonsin", updateList)
+			.then (r => {
+				return r.json();
+			})
+			.then (response => {
+				console.log(response);
+			});		
+
+
 			setTasks(newTasks);
 		}
 	}
